bb7e6c1372a7ffa3e6066c6d6a7bbb79db383c19
John Marc <167587949+jmSanchezzz@users.noreply.github.com>
2025-08-27 03:03:21 +0000

    Updated according to the required output

--- factoryPattern/Clinic.java
+++ factoryPattern/Clinic.java
@@ -1,5 +1,4 @@
 package factoryPattern;
-
 import java.util.Scanner;
 
 public class Clinic {
@@ -7,8 +6,10 @@ public class Clinic {
 
         Scanner input = new Scanner(System.in);
 
+while(true){
         System.out.println("[1] Dog");
         System.out.println("[2] Cat");
+        System.out.println("[3] Exit");
         System.out.print("\nChoose your pet number: ");
         Integer choice = input.nextInt();
 
@@ -21,19 +22,21 @@ public class Clinic {
                 petFile.setPetName("Bantay");
                 petFile.setPet(pet);
                 ((Dog) pet).setBreed("German Shepperd");
+                System.out.println(((Dog) pet).getBreed());
                 break;
             case 2: pet = new Cat();
                 petFile.setPetId("C01");
                 petFile.setPetName("Muning");
                 petFile.setPet(pet);
                 ((Cat) pet).setNoOfLives(9);
+                System.out.println(((Cat) pet).getNoOfLives());
+                break;
+            case 3:
+                System.out.println("Exiting program.....");
+                System.exit(0);
         }
-
-        System.out.println("Pet id is " + petFile.getPetId());
-        System.out.println("Pet name is " + petFile.getPetName());
-        System.out.println("Pet kind: " + petFile.getPet().getClass().getSimpleName());
-        System.out.println("Communication sound: "+ petFile.getPet().makeSound());
-        System.out.println("Play mode: " + petFile.getPet().play());
+    }
+        
 
     }
 }
